2e77bf192b63f2ec984f2c24465e4887c1fbab45
Mafiaaaaaa <ss4113748@gmil.com>
2024-04-11 05:16:04 +0500

    first commit

--- index.js
+++ index.js
@@ -1,26 +1,80 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
 let todoList = [];
-let conditions = true;
-console.log("\n\tWelcome to Shaheer's TO DO List Application\n");
-while (conditions) {
-    let addTask = await inquirer.prompt([
-        {
-            name: "Task",
-            type: "input",
-            message: "Enter your new task"
+let conditions = true || false;
+console.log(chalk.bold.italic.yellow("\n\tWelcome to Shaheer's TO DO List Application\n"));
+let main = async () => {
+    while (conditions) {
+        let frontPage = await inquirer.prompt([
+            {
+                name: "Options",
+                type: "list",
+                message: "Please select from the options given below",
+                choices: ["Add Task", "Delete Task", "Edit Task", "View Tasks", "Exit"]
+            }
+        ]);
+        if (frontPage.Options === "Add Task") {
+            await addTask();
         }
-    ]);
-    todoList.push(addTask.Task);
-    console.log(`Your ${addTask.Task} Task Added In Your TO DO List.`);
+        else if (frontPage.Options === "Delete Task") {
+            await deleteTask();
+        }
+        else if (frontPage.Options === "Edit Task") {
+            await editTask();
+        }
+        else if (frontPage.Options === "View Tasks") {
+            await viewTask();
+        }
+        else if (frontPage.Options === "Exit") {
+            conditions = false;
+        }
+    }
+};
+let addTask = async () => {
     let newTask = await inquirer.prompt([
         {
-            name: "New_Task",
-            type: "confirm",
-            message: "Do You Want to Add New Task",
-            default: "False"
+            name: "newtask",
+            type: "input",
+            message: "Please Enter your new Task"
         }
     ]);
-    conditions = newTask.New_Task;
-}
-console.log(`Your New TO DO List is ${todoList}`);
+    todoList.push(newTask.newtask);
+    console.log(chalk.green(`\n${newTask.newtask} Task added Successfully\n`));
+};
+let deleteTask = async () => {
+    await viewTask();
+    let deleteIndex = await inquirer.prompt([
+        {
+            name: "index",
+            type: "number",
+            message: "Please Enter the Number of the Task you want to Delete"
+        }
+    ]);
+    let deleteTask = todoList.splice(deleteIndex.index - 1, 1);
+    console.log(chalk.red(`\nTask ${deleteIndex.index - 1} has been Deleted from your List`));
+};
+let editTask = async () => {
+    await viewTask();
+    let editIndex = await inquirer.prompt([
+        {
+            name: "index",
+            type: "number",
+            message: "Please Enter the Number of the Task you want to Edit "
+        },
+        {
+            name: "editedTask",
+            type: "input",
+            message: "Your Edited Task: "
+        }
+    ]);
+    todoList[editIndex.index - 1] = editIndex.editedTask;
+    console.log(chalk.green(`\nTask ${editIndex.index - 1} has been Edited from your List`));
+};
+let viewTask = () => {
+    console.log(chalk.green("\n Your To-Do List is\n"));
+    todoList.forEach((newtask, index) => {
+        console.log(chalk.blue(index + 1, newtask));
+    });
+};
+main();
--- index.ts
+++ index.ts
@@ -1,31 +1,90 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
 
-let todoList = [];
-let conditions = true;
-console.log("\n\tWelcome to Shaheer's TO DO List Application\n");
+let todoList:string []= [];
+let conditions = true || false;
+console.log(chalk.bold.italic.yellow("\n\tWelcome to Shaheer's TO DO List Application\n"));
 
+let main = async() => {
 while(conditions){
-    let addTask = await inquirer.prompt([
+    let frontPage = await inquirer.prompt([
         {
+            name: "Options",
+            type: "list",
+            message: "Please select from the options given below",
+            choices: ["Add Task", "Delete Task", "Edit Task", "View Tasks", "Exit"]       
+        }
+    
+]);
 
-            name: "Task",
+if (frontPage.Options === "Add Task"){
+    await addTask()
+}
+else if (frontPage.Options === "Delete Task"){
+    await deleteTask()
+}
+else if (frontPage.Options === "Edit Task"){
+    await editTask()
+}
+else if (frontPage.Options === "View Tasks"){
+    await viewTask()
+}
+else if (frontPage.Options === "Exit"){
+    conditions = false;
+}
+}
+}
+
+let addTask = async() => {
+    let newTask = await inquirer.prompt([
+        {
+            name: "newtask",
             type: "input",
-            message: "Enter your new task"
-    }
+            message: "Please Enter your new Task"
+        }
 ]);
- todoList.push(addTask.Task);
-console.log(`Your ${addTask.Task} Task Added In Your TO DO List.`);
-
-let newTask = await inquirer.prompt([
-    {
-        name: "New_Task",
-        type: "confirm",
-        message: "Do You Want to Add New Task",
-        default: "False"
-}
-]);
-conditions = newTask.New_Task;
+todoList.push(newTask.newtask);
+console.log(chalk.green(`\n${newTask.newtask} Task added Successfully\n`));
 }
 
-console.log(`Your New TO DO List is ${todoList}`);
+let deleteTask = async() => {
+    await viewTask() 
+     let deleteIndex = await inquirer.prompt([
+         {
+             name: "index",
+             type: "number",
+             message: "Please Enter the Number of the Task you want to Delete"
+         }
+     ]);
+ 
+        let deleteTask = todoList.splice(deleteIndex.index -1, 1);
+         console.log(chalk.red(`\nTask ${deleteIndex.index -1} has been Deleted from your List`));
+ }
+
+let editTask = async() => {
+   await viewTask()
+    let editIndex = await inquirer.prompt([
+        {
+            name: "index",
+            type: "number",
+            message: "Please Enter the Number of the Task you want to Edit "
+        },
+        {
+            name: "editedTask",
+            type: "input",
+            message: "Your Edited Task: "
+        }
+    ]);
+todoList[editIndex.index -1] = editIndex.editedTask
+console.log(chalk.green(`\nTask ${editIndex.index -1} has been Edited from your List`))
+}
+
+let viewTask = () => {
+    console.log(chalk.green("\n Your To-Do List is\n"));
+    todoList.forEach((newtask, index) => {
+        console.log(chalk.blue(index +1, newtask))
+    });
+}
+
+main();
